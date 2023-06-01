343c540c90854beff9480790b2b490e3b848a651
Logan Luo <logan.luo@cronoslabs.org>
2023-06-01 14:14:06 +0800

    update linked_list4

--- src/linked_list4.rs
+++ src/linked_list4.rs
@@ -52,26 +52,38 @@ impl<T> List<T> {
         }
     }
 
+    pub fn peek(&self) -> Option<&T> {
+        unsafe {
+            self.head
+                .as_ref()
+                .map(|head| &head.elem)
+        }
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        unsafe {
+            self.head
+                .as_mut()
+                .map(|head| &mut head.elem)
+        }
+    }
+
     pub fn into_iter(self) -> IntoIter<T> {
         IntoIter(self)
     }
 
     pub fn iter(&self) -> Iter<T> {
-        if self.head.is_null() {
-            Iter { next: None }
-        } else {
+        unsafe {
             Iter {
-                next: unsafe { Some(&*self.head) },
+                next: self.head.as_ref(),
             }
         }
     }
 
     pub fn iter_mut(&mut self) -> IterMut<T> {
-        if self.head.is_null() {
-            IterMut { next: None }
-        } else {
+        unsafe {
             IterMut {
-                next: unsafe { Some(&mut *self.head) },
+                next: self.head.as_mut(),
             }
         }
     }
@@ -104,7 +116,7 @@ impl<'a, T> Iterator for Iter<'a, T> {
             None => None,
             Some(node) => {
                 if !node.next.is_null() {
-                    self.next = unsafe { Some(&*node.next) };
+                    self.next = unsafe { node.next.as_ref() };
                 }
 
                 Some(&node.elem)
@@ -124,7 +136,7 @@ impl<'a, T> Iterator for IterMut<'a, T> {
             None => None,
             Some(node) => {
                 if !node.next.is_null() {
-                    self.next = unsafe { Some(&mut *node.next) };
+                    self.next = unsafe { node.next.as_mut() };
                 }
 
                 Some(&mut node.elem)
@@ -141,37 +153,65 @@ mod test {
     fn basics() {
         let mut list = List::new();
 
-        // Check empty list behaves right
         assert_eq!(list.pop(), None);
 
-        // Populate list
         list.push(1);
         list.push(2);
         list.push(3);
 
-        // Check normal removal
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), Some(2));
 
-        // Push some more just to make sure nothing's corrupted
         list.push(4);
         list.push(5);
 
-        // Check normal removal
         assert_eq!(list.pop(), Some(3));
         assert_eq!(list.pop(), Some(4));
 
-        // Check exhaustion
         assert_eq!(list.pop(), Some(5));
         assert_eq!(list.pop(), None);
 
-        // Check the exhaustion case fixed the pointer right
         list.push(6);
         list.push(7);
 
-        // Check normal removal
         assert_eq!(list.pop(), Some(6));
         assert_eq!(list.pop(), Some(7));
         assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert!(list.pop() == Some(1));
+        list.push(4);
+        assert!(list.pop() == Some(2));
+        list.push(5);
+
+        assert!(list.peek() == Some(&3));
+        list.push(6);
+        list.peek_mut().map(|x| *x *= 10);
+        assert!(list.peek() == Some(&30));
+        assert!(list.pop() == Some(30));
+
+        for elem in list.iter_mut() {
+            *elem *= 100;
+        }
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&400));
+        assert_eq!(iter.next(), Some(&500));
+        assert_eq!(iter.next(), Some(&600));
+        assert_eq!(iter.next(), None);
+        assert_eq!(iter.next(), None);
+
+        assert!(list.pop() == Some(400));
+        list.peek_mut().map(|x| *x *= 10);
+        assert!(list.peek() == Some(&5000));
+        list.push(7);
     }
 }
